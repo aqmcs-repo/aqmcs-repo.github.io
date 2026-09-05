56ed937511fb6b32642d1dafd66e9028d293c4fc
aqmcs-repo <aquimismoconveniencestore@gmail.com>
2026-09-05 12:40:21 -0400

    Create products.js

---
+++ data/products.js
@@ -0,0 +1,28 @@
+const productosData = {
+    "PRODUCTS": [
+        {
+            "ID": 000001,
+            "CATEGORY": "CERVEZAS",
+            "UNIT": "LATA",
+            "NAME": "Cerveza La Bala 330ml",
+            "EXPIRE_DATE": "0000-00-00",
+            "COUNTRY": "UNKNOWN",
+            "OFERT_PRICE": 0,
+            "STANDARD_PRICE": 410,
+            "IS_OFERT": false,
+            "IS_NEW": false
+        }
+        {
+            "ID": 000002,
+            "CATEGORY": "BEBIDAS ALCOHÓLICAS",
+            "UNIT": "BOTELLA",
+            "NAME": "Whisky John Mail 1L (Red Etiquette)",
+            "EXPIRE_DATE": "0000-00-00",
+            "COUNTRY": "UNKNOWN",
+            "OFERT_PRICE": 0,
+            "STANDARD_PRICE": 3800,
+            "IS_OFERT": false,
+            "IS_NEW": false
+        }
+    ]
+}
